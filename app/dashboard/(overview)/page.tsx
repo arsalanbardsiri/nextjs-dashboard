b729c73c1738c9ee4dfdb78d2d0968137a0d056b
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2025-05-27 15:39:36 -0700

    Search & Sort - hooks for client - proper funtion for server

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,22 +6,24 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons'
 import CardWrapper from "@/app/ui/dashboard/cards"
+import { Fragment } from 'react'
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
   // const { numberOfInvoices, totalPaidInvoices, totalPendingInvoices, numberOfCustomers } = await fetchCardData()
 
   return (
-    <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
-      </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
-        </Suspense>
+    <>
+      <main>
+        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+          Dashboard
+        </h1>
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          <Suspense fallback={<CardsSkeleton />}>
+            <CardWrapper />
+          </Suspense>
 
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+          {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card
@@ -29,17 +31,18 @@ export default async function Page() {
           value={numberOfCustomers}
           type="customers"
         /> */}
-      </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
-        </Suspense>
+        </div>
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+          <Suspense fallback={<RevenueChartSkeleton />}>
+            <RevenueChart />
+          </Suspense>
 
-        {/* <RevenueChart revenue={revenue}  /> */}
-        <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
-        </Suspense>
-      </div>
-    </main>
+          {/* <RevenueChart revenue={revenue}  /> */}
+          <Suspense fallback={<LatestInvoicesSkeleton />}>
+            <LatestInvoices />
+          </Suspense>
+        </div>
+      </main>
+    </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
+import { experimental_taintObjectReference } from 'react';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental:{
+  //   ppr: "incremental"
+  // }
 };
 
 export default nextConfig;
